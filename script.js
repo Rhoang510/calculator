3b01c9dee8af8f12a457ebfd6b8b08252748008a
Roger <rhoang510@gmail.com>
2021-12-31 12:21:03 -0800

    Few changes

--- script.js
+++ script.js
@@ -1,14 +1,9 @@
-let ops = [
-    "+",
-    "-", 
-    "*", 
-    "/"
-];
-
+let ops = [];
 let firstInput = [];
 let secondInput = [];
+let total = parseInt(0);
 const numberBtn = document.querySelectorAll(".numbers");
-const operationBtn = document.querySelectorAll(".operation");
+const operationBtn = document.querySelectorAll(".ops");
 const bottomDisplay = document.querySelector(".currentNumber");
 const topDisplay = document.querySelector(".previousNumber");
 const clearBtn = document.querySelector("#clearBtn");
@@ -19,6 +14,7 @@ const addBtn = document.querySelector("#addBtn");
 const subBtn = document.querySelector("#subBtn");
 const multiplyBtn = document.querySelector("#multiplyBtn");
 const divideBtn = document.querySelector("#divideBtn");
+let opsBtn = document.querySelector(".ops");
 
 
 function add(a, b) {
@@ -41,20 +37,23 @@ function divide(a, b) {
 //     return array.reduce((total, current) => total + current, 0);
 //   };
 
-function operate(firstInput, secondInput) {
-    let total = parseInt(0);
+function equal() {
     switch(ops) {
         case "+":
-            total = add(userInput);
+            total = add(firstInput, secondInput);
+            bottomDisplay.textContent = total;
             break;
         case "-":
-            total = subtract(userInput);
+            total = subtract(firstInput, secondInput);
+            bottomDisplay.textContent = total;
             break;
         case "*":
-            total = multiply(userInput);
+            total = multiply(firstInput, secondInput);
+            bottomDisplay.textContent = total;
              break;
-        case "/":
-            total = divide(userInput);
+        case "÷":
+            total = divide(firstInput, secondInput);
+            bottomDisplay.textContent = total;
             break;
     }
 }
@@ -70,6 +69,24 @@ numberBtn.forEach((number) => {
     number.addEventListener("click", pullNum);
 });
 
+function pickOperator(e) {
+    let operator = this.id;
+    ops.push(operator);
+    console.log(operator);
+}
+
+
+equalBtn.addEventListener("click", operate);
+
 operationBtn.forEach((ops) => {
-    ops.addEventListener("click", operate);
+    ops.addEventListener("click", pickOperator)
+})
+
+clearBtn.addEventListener("click", () => {
+    ops = [];
+    firstInput = [];
+    secondInput = [];
+    total = parseInt(0);
+    topDisplay.textContent = "";
+    bottomDisplay.textContent = "";
 })
