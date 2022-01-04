65eee261f9e93f90cec2ce2ee845793265adb43b
Roger <rhoang510@gmail.com>
2022-01-04 13:51:05 -0800

    Changes to functions

--- script.js
+++ script.js
@@ -1,29 +1,27 @@
 let operator = "";
-let storedNumber = "";
-let currentNumber = "";
-let total = 0;
+let storedNumber = [];
+let currentNumber = [];
+let total = "";
 const numberBtn = document.querySelectorAll(".numbers");
-const operationBtn = document.querySelectorAll(".ops");
 const bottomDisplay = document.querySelector(".currentNumber");
 const topDisplay = document.querySelector(".previousNumber");
 const clearBtn = document.querySelector("#clearBtn");
 const equalBtn = document.querySelector("#equalBtn");
 const decimalBtn = document.querySelector("#decimalBtn");
 const deleteBtn = document.querySelector("#deleteBtn");
+const remainderBtn = document.querySelector("#remainder")
 const opsBtn = document.querySelectorAll(".ops");
-// const addBtn = document.querySelector("#addBtn");
-// const subBtn = document.querySelector("#subBtn");
-// const multiplyBtn = document.querySelector("#multiplyBtn");
-// const divideBtn = document.querySelector("#divideBtn");
+// const pressedOperator = Array.from(document.querySelectorAll("#ops"))
 
 function calculate(storedNumber, currentNumber, operator) {
     let operators = {
         "+": (a, b) => a + b,
         "-": (a, b) => a - b,
         "x": (a, b) => a * b,
-        "÷": (a, b) => a / b
+        "÷": (a, b) => b === 0 ? "Error, can't divide by 0" : a / b,
+        "%": (a, b) => a % b
     }
-    return operator in operators ? operators[operator](parseInt(storedNumber), parseInt(currentNumber)) : NaN;
+     return operator in operators ? operators[operator](parseInt(storedNumber), parseInt(currentNumber)) : NaN; 
 }
 
 function pullNumber() {
@@ -33,38 +31,39 @@ function pullNumber() {
     console.log(currentNumber);
 }
 
-// function pickOperator() {
-//     let operator = this.textContent;
-//     storedNumber = currentNumber;
-//     currentNumber = "";
-//         switch(operator) {
-//             case "+":
-//                 bottomDisplay.textContent = "";
-//                 topDisplay.textContent = `${storedNumber} ${this.id}`
-//                 console.log(operator);
-//                 break;
-//             case "-":
-//                 bottomDisplay.textContent = "";
-//                 topDisplay.textContent = `${storedNumber} ${this.id}`
-//                 break;
-//             case "x":
-//                 bottomDisplay.textContent = "";
-//                 topDisplay.textContent = `${storedNumber} ${this.id}`
-//                 break;
-//             case "÷":
-//                 bottomDisplay.textContent = "";
-//                 topDisplay.textContent = `${storedNumber} ${this.id}`
-//                 break;
-//         }
-//     }
-
 function pickOperator() {
     operator = this.textContent;
     storedNumber = currentNumber;
-    currentNumber = "";
+    currentNumber = [];
     bottomDisplay.textContent = "";
     topDisplay.textContent = `${storedNumber} ${operator}`;
     console.log(operator);
+}
+
+function calculatingTotal() {
+    total = calculate(storedNumber, currentNumber, operator);
+    bottomDisplay.textContent = total;
+    topDisplay.textContent = `${storedNumber} ${operator} ${currentNumber}`
+    currentNumber = total;
+    currentNumber = "";
+    operator = "";
+}
+
+function clear() {
+    operator = [];
+    storedNumber = [];
+    currentNumber = [];
+    total = 0;
+    topDisplay.textContent = "";
+    bottomDisplay.textContent = "";
+}
+
+function erase() {
+
+}
+
+function decimal() {
+
 }
 
 numberBtn.forEach((number) => {
@@ -72,16 +71,10 @@ numberBtn.forEach((number) => {
  });
 
  opsBtn.forEach((op) => {
-     op.addEventListener("click", pickOperator);
+    op.addEventListener("click", pickOperator);
  });
 
-equalBtn.addEventListener("click", calculate);
-
-clearBtn.addEventListener("click", () => {
-    operator = "";
-    firstInput = "";
-    secondInput = "";
-    total = 0;
-    topDisplay.textContent = "";
-    bottomDisplay.textContent = "";
-})
+equalBtn.addEventListener("click", calculatingTotal);
+clearBtn.addEventListener("click", clear);
+// decimalBtn.addEventListener("click", decimal);
+// deleteBtn.addEventListener("click", erase);
